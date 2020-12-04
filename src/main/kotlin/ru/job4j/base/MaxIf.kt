131ca1726eb409a7da077a3cc5aebd7fa14395bf
dmitrytishchenko <tdmitiu@mail.ru>
2020-12-04 12:41:57 +0300

    4. Операторы условий if [#414853] ver2

--- src/main/kotlin/ru/job4j/base/MaxIf.kt
+++ src/main/kotlin/ru/job4j/base/MaxIf.kt
@@ -2,4 +2,4 @@ package ru.job4j.base
 
 fun max(first : Int, second : Int) = if (first > second) first else second
 
-fun max3(first : Int, second : Int, third : Int) = if (first > if(second > third) second else third) first else if(second > third) second else third
+fun max3(first : Int, second : Int, third : Int) = max(first, (max(second, third)))
